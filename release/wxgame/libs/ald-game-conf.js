6805ae6c355d151d67d866067e8fac95569aa995
Atlaz <atlaz@163.com>
2019-06-27 17:12:15 +0800

    [ v1.2.1 ] 1. 增加一个新场景-浮岛：第三大关开启，每一大关轮换； 2. 更换appid；

--- release/wxgame/libs/ald-game-conf.js
+++ release/wxgame/libs/ald-game-conf.js
@@ -1,3 +1,2 @@
 exports.app_key = "b37d9400b8b494b067a438eba4573926"; //请在此行填写从阿拉丁后台获取的appkey
 exports.getLocation = false; //默认不获取用户坐标位置
-exports.useOpen = true; //默认不启用，是否启用openid计算，开启后必须上传openid，否则数据不会上报。
